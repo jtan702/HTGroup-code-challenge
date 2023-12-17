0ae64f29dfff701d45844ab48b7a64a3fcd7fdc5
Tan Jiajun <jiajuntan702@gmail.com>
2023-12-18 03:13:39 +0800

    fixed bug with output

--- crispy-dallop.py
+++ crispy-dallop.py
@@ -1,18 +1,6 @@
 '''
-12 = 21
-513 = 531
-2017 = 2071
-9 = -1
-111 = -1
-531 = -1
-0 = -1
-123 = 132
-215430 = 230145
-4321 = -1
-[12,513,2017,9,111,531,0,123,215430,4321]
+assuming that input 'num' is a positive integer
 '''
-
-# assuming that input 'num' is a positive integer
 def nextNumber(num):
     out = -1
     break_index = None
@@ -30,12 +18,14 @@ def nextNumber(num):
                 if num_arr[break_index] < num_arr[j]:
                     num_arr[j], num_arr[break_index] = num_arr[break_index],num_arr[j]
                     break
-            out = ''.join([str(x) for x in num_arr])
+            out = int(''.join([str(x) for x in num_arr]))
             break
 
     return out
 
 
 tests = [12,513,2017,9,111,531,0,123,215430,4321,000]
-for test in tests:
-    print(nextNumber(test))
+answers = [21,531,2071,-1,-1,-1,-1,132,230145,-1,-1,-1]
+for i in range(len(tests)):
+    out = nextNumber(tests[i])
+    print( out == answers[i], out )
